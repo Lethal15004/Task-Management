e6fee340d979c3046c56f2393fe20187ad8dc314
Lethal15004 <Phamvahuy2004@gmail.com>
2024-08-24 17:57:43 +0700

    Sắp xếp theo tiêu chí

--- controller/client/task.controller.js
+++ controller/client/task.controller.js
@@ -3,10 +3,14 @@ module.exports.index=async(req,res)=>{
     const find={
         deleted:false
     }
+    const sort={};
     if(req.query.status){
         find.status=req.query.status;
     }
-    const tasks = await Task.find(find);
+    if(req.query.sortKey && req.query.sortValue){
+        sort[req.query.sortKey]=req.query.sortValue;
+    }
+    const tasks = await Task.find(find).sort(sort);
     res.json(tasks);
 }
 module.exports.detail=async(req,res)=>{
